10847d7e3da6a78176cd104b9222dfd8df62e7a2
Nikita Silaev <Beeeirym@gmail.com>
2024-11-06 22:51:39 +0300

    feat: Changed the logic of filtering the list of orders and optimize the SQL query for selection

--- src/jackhelper/orders/orders_list.py
+++ src/jackhelper/orders/orders_list.py
@@ -24,7 +24,12 @@ def getOrdersCountAndList(
     '''
 
     orders = []
-    where_conditions, having_conditions, params_values = makeQueryConditionsList(search, tags)
+
+    having_conditions, params_values = makeQueryConditionsList(search, tags)
+    if having_conditions:
+        having_conditions_string = f"HAVING {' AND '.join(having_conditions)}"
+    else:
+        having_conditions_string = ''
 
     connect = autodealer.getConnect(city)
     cursor = connect.cursor()
@@ -32,28 +37,30 @@ def getOrdersCountAndList(
         SELECT doh.FULLNUMBER, 
             doh.DATE_CREATE, 
             c.FULLNAME, 
-            FLOOR(AVG(sw.DISCOUNT_WORK)) AS AVG_DISCOUNT_WORK
+            sw.AVG_DISCOUNT_WORK
         FROM DOCUMENT_OUT_HEADER doh
-        LEFT JOIN DOCUMENT_SERVICE_DETAIL ds
-            ON doh.DOCUMENT_OUT_HEADER_ID = ds.DOCUMENT_OUT_HEADER_ID
-        LEFT JOIN DOCUMENT_OUT do 
+        JOIN DOCUMENT_OUT do 
             ON doh.DOCUMENT_OUT_ID = do.DOCUMENT_OUT_ID
-        LEFT JOIN SERVICE_WORK sw 
-            ON doh.DOCUMENT_OUT_ID = sw.DOCUMENT_OUT_ID
         JOIN CLIENT c 
             ON do.CLIENT_ID = c.CLIENT_ID
+        LEFT JOIN (
+            SELECT DOCUMENT_OUT_ID, FLOOR(AVG(DISCOUNT_WORK)) AS AVG_DISCOUNT_WORK
+            FROM SERVICE_WORK
+            GROUP BY DOCUMENT_OUT_ID
+        ) sw ON doh.DOCUMENT_OUT_ID = sw.DOCUMENT_OUT_ID
+        LEFT JOIN DOCUMENT_SERVICE_DETAIL ds
+            ON doh.DOCUMENT_OUT_HEADER_ID = ds.DOCUMENT_OUT_HEADER_ID
         WHERE (doh.DATE_CREATE BETWEEN timestamp '{start_date} 00:00' AND timestamp '{end_date} 23:59')
             AND doh.DOCUMENT_TYPE_ID = 11
             AND doh.STATE = 4
-            {where_conditions}
-        GROUP BY doh.FULLNUMBER, doh.DATE_CREATE, c.FULLNAME
+        GROUP BY doh.FULLNUMBER, doh.DATE_CREATE, c.FULLNAME, sw.AVG_DISCOUNT_WORK,
+                ds.SPECIAL_NOTES, ds.RUN_DURING, ds.RUN_BEFORE, ds.REASONS_APPEAL
         {having_conditions}
-        ORDER BY doh.DATE_CREATE
+        ORDER BY doh.DATE_CREATE;
     '''.format(
         start_date=start_date, 
         end_date=end_date,
-        where_conditions=("AND " + "\nAND ".join(where_conditions) if where_conditions else ''),  
-        having_conditions=("HAVING " + "\nAND ".join(having_conditions) if having_conditions else ''),  
+        having_conditions=having_conditions_string,  
     )
     raw_orders = cursor.execute(orders_list_query, params_values).fetchall()
     connect.close()
@@ -86,46 +93,28 @@ def getOrdersCountAndList(
     return orders_count, orders
 
 
-def makeQueryConditionsList(search, tags) -> tuple[list, list, list]:
+def makeQueryConditionsList(search, tags) -> tuple[list, list]:
     '''Creates a list of conditions for SQL orders selection query based on search and tags.'''
 
-    where_conditions, having_conditions, params_values = [], [], []
+    having_conditions, params_values = [], []
 
     if search:
         if len(search) > 21:
             raise ValueError('Максимальная длина поискового запроса - 21 символ.')
         search_condition = "(doh.FULLNUMBER LIKE '%' || ? || '%')"
-        where_conditions.append(search_condition)
+        having_conditions.append(search_condition)
         params_values.append(search.upper())
 
     if tags:
         tags_conditions = {
-            'without_recommendations': {
-                "condition": "(ds.SPECIAL_NOTES IS NULL OR CHAR_LENGTH(ds.SPECIAL_NOTES) < 20)",
-                "sql_block": "WHERE"
-            },
-            'without_milleage': {
-                "condition": "(ds.RUN_DURING IS NULL OR ds.RUN_BEFORE IS NULL)",
-                "sql_block": "WHERE"
-            },
-            'without_reasons_appeal': {
-                "condition": "(ds.REASONS_APPEAL IS NULL)",
-                "sql_block": "WHERE"
-            },
-            'with_discount_lte_10': {
-                "condition": "FLOOR(AVG(sw.DISCOUNT_WORK)) > 0 AND FLOOR(AVG(sw.DISCOUNT_WORK)) <= 10",
-                "sql_block": "HAVING"
-            },
-            'with_discount_gte_11': {
-                "condition": "FLOOR(AVG(sw.DISCOUNT_WORK)) >= 11",
-                "sql_block": "HAVING"
-            },
+            'without_recommendations': "(ds.SPECIAL_NOTES IS NULL OR CHAR_LENGTH(ds.SPECIAL_NOTES) < 20)",
+            'without_milleage': "(ds.RUN_DURING IS NULL OR ds.RUN_BEFORE IS NULL)",
+            'without_reasons_appeal': "(ds.REASONS_APPEAL IS NULL)",
+            'with_discount_lte_10': "(sw.AVG_DISCOUNT_WORK > 0 AND sw.AVG_DISCOUNT_WORK <= 10)",
+            'with_discount_gte_11': "(sw.AVG_DISCOUNT_WORK >= 11)",
         }
-        for tag in tags:
-            if tag in tags_conditions.keys(): 
-                tag_data = tags_conditions[tag]
-                match tag_data['sql_block']:
-                    case 'WHERE': where_conditions.append(tag_data['condition'])
-                    case 'HAVING': having_conditions.append(tag_data['condition'])
+        selected_tags_conditions = [tags_conditions[tag] for tag in tags if tag in tags_conditions.keys()]
+        tags_condition_string = f"({' OR '.join(selected_tags_conditions)})"
+        having_conditions.append(tags_condition_string)
 
-    return where_conditions, having_conditions, params_values
+    return having_conditions, params_values
